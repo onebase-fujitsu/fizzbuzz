e64caaf16ab1c9f08369fb0c23ba2a4b8d1f4246
dev1track <toast20191106Dev1@gmail.com>
2025-05-27 13:55:30 +0900

    TypeScriptディレクトリを作成

---
+++ Typescript/test/Game.spec.ts
@@ -0,0 +1,13 @@
+import { FizzBuzz } from "../src/FizzBuzz"
+
+describe("FizzBuzz", () => {
+    it("数字の1を入力すると文字列の1が返ってくる", () => {
+        const fizzBuzz = new FizzBuzz()
+    })
+
+    it("数字の3の倍数を入力すると文字列のFizzが返ってくる", () => {})
+
+    it("数字の5の倍数を入力すると文字列のBuzzが返ってくる", () => {})
+
+    it("数字の3と5の倍数を入力すると文字列のFizzBuzzが返ってくる", () => {})
+})
