3680bade0c132c4918c2b2d13e90465f9288bce4
dev1track <toast20191106Dev1@gmail.com>
2025-05-29 12:49:58 +0900

    スタートの状態にするために実装・テストを削除

--- Java/src/main/java/FizzBuzz.java
+++ Java/src/main/java/FizzBuzz.java
@@ -1,13 +1,2 @@
 public class FizzBuzz {
-    public String getResponse(int inputValue) {
-        if (inputValue % 15 == 0) {
-            return "FizzBuzz";
-        } else if (inputValue % 3 == 0) {
-            return "Fizz";
-        } else if (inputValue % 5 == 0) {
-            return "Buzz";
-        } else {
-            return String.valueOf(inputValue);
-        }
-    }
 }
--- Java/src/test/java/FizzBuzzTest.java
+++ Java/src/test/java/FizzBuzzTest.java
@@ -10,35 +10,13 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void 数字の2を入力すると文字列の2が返ってくる() {
-        FizzBuzz fb = new FizzBuzz();
-        assertThat(fb.getResponse(2)).isEqualTo("2");
-    }
+    void 数字の2を入力すると文字列の2が返ってくる() {}
 
     @Test
     void 数字の3の倍数を入力すると文字列のFizzが返ってくる() {
         FizzBuzz fb = new FizzBuzz();
-        assertThat(fb.getResponse(3)).isEqualTo("Fizz");
-        assertThat(fb.getResponse(6)).isEqualTo("Fizz");
-        assertThat(fb.getResponse(9)).isEqualTo("Fizz");
-        assertThat(fb.getResponse(12)).isEqualTo("Fizz");
-        assertThat(fb.getResponse(15)).contains("Fizz");
-    }
-
-    @Test
-    void 数字の5の倍数を入力すると文字列のBuzzが返ってくる() {
-        FizzBuzz fb = new FizzBuzz();
-        assertThat(fb.getResponse(5)).isEqualTo("Buzz");
-        assertThat(fb.getResponse(10)).isEqualTo("Buzz");
-        assertThat(fb.getResponse(15)).contains("Buzz");
-    }
-
-    @Test
-    void 数字の3と5の倍数を入力すると文字列のFizzBuzzが返ってくる() {
-        FizzBuzz fb = new FizzBuzz();
-        assertThat(fb.getResponse(15)).contains("FizzBuzz");
-        assertThat(fb.getResponse(30)).contains("FizzBuzz");
-        assertThat(fb.getResponse(45)).contains("FizzBuzz");
-        assertThat(fb.getResponse(60)).contains("FizzBuzz");
+        // 3を入力した場合
+        // 6を入力した場合
+        // 9を入力した場合
     }
 }
